f181d43e4e6f63c90b9dbfbf6e28cdc764e2cec4
Bruno Davi <48166556+brunodavi@users.noreply.github.com>
2024-11-10 03:56:16 -0300

    chore: config linter and formmater

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,31 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default [
   {
     languageOptions: {
-      globals: {...globals.browser, ...globals.node}
-    }
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
   },
 
   pluginJs.configs.recommended,
-];
+  eslintPluginPrettierRecommended,
+
+  {
+    files: ['**/src/*', '**/tests/*'],
+  },
+
+  {
+    ignores: ['**/node_modules/*', '**/dist/*'],
+  },
+
+  {
+    rules: {
+      semi: ['error', 'never'],
+    },
+  },
+]
